8a7acf4bbe91256a2b80900d689057f0ecb742c4
Owethu99Twala <owethutwala99@gmail.com>
2020-07-30 10:50:03 +0200

    React complete Redux to go

---
+++ underscores/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './components/Underscores'
+import Underscores from './components/Underscores';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <Underscores />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ underscores/src/components/Underscores.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Underscores extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numbers: undefined,
+            results: undefined
+        }
+
+    }
+    onChange = e => {
+        e.preventDefault()
+        this.setState({
+            numbers: e.target.value
+        })
+    }
+
+    returnUnderscores = () => {
+     var {numbers} = this.state;
+        var odds;
+        var evens;
+        var newArr = []
+    
+        for (var i = 0; i < numbers.length; i++) {
+            if ((numbers[i] % 2) != 1) {
+                evens = numbers[i] + "_";
+                newArr.push(evens)
+            }
+            else {
+                var odds = numbers[i] ;
+                newArr.push(odds)
+            }
+        }
+        this.setState({
+            results: newArr
+        })
+        return newArr;
+    }
+    render() {
+        const {results} = this.state;
+        return (
+            <div>
+                <h2>Add Underscores to even numbers</h2>
+                <input type="number" onChange={this.onChange}/>
+                <button onClick={this.returnUnderscores}>Execute</button>
+                <br/>
+                <label >Results: </label>
+                {results}
+            </div>
+        );
+    }
+}
+
+Underscores.propTypes = {
+
+};
+
+export default Underscores;
